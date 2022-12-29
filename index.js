55c19df9ac4540f5f22ea63cf87d2c5b0c85868e
mhurchaidh <bmurchie1997@gmail.com>
2022-12-29 15:27:17 -0800

    Added a list for each image date and functionality to render that dates image when clicked

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const URL = "http://localhost:3000/space-pictures";
 const dailyURL = "https://go-apod.herokuapp.com/apod";
 
 const dailyImage = document.querySelector('#daily-image');
+const pictureDatesList = document.querySelector('#picture-dates-list');
 
 
 function getPicture(url){
@@ -9,13 +10,27 @@ function getPicture(url){
         .then(resp => resp.json())
 }
 
+function iteratePictures(pictureArray){
+    pictureArray.forEach(addPictures);
+}
+
+function addPictures(picObject){
+    const dateItem = document.createElement('li');
+    dateItem.textContent = picObject.date;
+    dateItem.addEventListener('click', () => {
+        dailyImage.innerHTML = '';
+        renderPicture(picObject)});
+    pictureDatesList.append(dateItem);
+}
+
 function renderPicture(spaceObject){
-    image = document.createElement('img');
+    const image = document.createElement('img');
     image.setAttribute('id', 'space-image');
     image.setAttribute('src', spaceObject.hdurl);
     dailyImage.append(image);
 }
 
-getPicture(dailyURL).then(object => {
-    renderPicture(object);
+getPicture(URL).then(pictureArray => {
+    iteratePictures(pictureArray);
+    renderPicture(pictureArray[pictureArray.length -1]);
 })
