846ea5d182c55a692a8d70c2721d8553c990ca1b
mhurchaidh <bmurchie1997@gmail.com>
2022-12-28 17:20:44 -0800

    Added script link in html file and renderImage function in index.js

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const dailyURL = "https://go-apod.herokuapp.com/apod";
 
 const dailyImage = document.querySelector('#daily-image');
 
+
 function getPicture(url){
     return fetch(url)
         .then(resp => resp.json())
 }
 
-getPicture(dailyURL).then(console.log);
+function renderPicture(spaceObject){
+    image = document.createElement('img');
+    image.setAttribute('id', 'space-image');
+    image.setAttribute('src', spaceObject.hdurl);
+    dailyImage.append(image);
+}
+
+getPicture(dailyURL).then(object => {
+    renderPicture(object);
+})
