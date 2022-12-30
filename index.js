5df79c32db7415b923fe529052398bca8cd41426
mhurchaidh <bmurchie1997@gmail.com>
2022-12-29 16:44:21 -0800

    Added pictureinfo into renderPicture function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dailyURL = "https://go-apod.herokuapp.com/apod";
 
 const dailyImage = document.querySelector('#daily-image');
 const pictureDatesList = document.querySelector('#picture-dates-list');
-
+const pictureInfo = document.querySelector('#picture-info');
 
 function getPicture(url){
     return fetch(url)
@@ -16,9 +16,10 @@ function iteratePictures(pictureArray){
 
 function addPictures(picObject){
     const dateItem = document.createElement('li');
-    dateItem.textContent = picObject.date;
+    dateItem.textContent = `* ${picObject.date}`;
     dateItem.addEventListener('click', () => {
         dailyImage.innerHTML = '';
+        pictureInfo.innerHTML = '';
         renderPicture(picObject)});
     dateItem.addEventListener('mouseover', () => dateItem.style.color = 'cyan');
     dateItem.addEventListener('mouseout', () => dateItem.style.color = 'white');
@@ -27,9 +28,16 @@ function addPictures(picObject){
 
 function renderPicture(spaceObject){
     const image = document.createElement('img');
+    const title = document.createElement('h2');
+    const explanation = document.createElement('p');
+    const copyright = document.createElement('em');
+    copyright.textContent = `Copyright: ${spaceObject.copyright}`;
+    explanation.textContent = spaceObject.explanation;
+    title.textContent = spaceObject.title;
     image.setAttribute('id', 'space-image');
     image.setAttribute('src', spaceObject.hdurl);
     dailyImage.append(image);
+    pictureInfo.append(title, explanation, copyright);
 }
 
 getPicture(URL).then(pictureArray => {
